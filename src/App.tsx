d36ebc4fbbc9595ab63ded79752974b17d6b5297
dayongbz <dayongbz@gmail.com>
2020-08-25 22:52:11 +0900

    gh pages setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useState, useRef, memo, useEffect } from 'react';
-import './reset.css'
-import './App.css';
-import Dot from './components/Dot'
+import React, { useState, useRef, memo, useEffect } from "react";
+import "./reset.css";
+import "./App.css";
+import Dot from "./components/Dot";
 
 const App = () => {
   const [name, setName] = useState<string>("chaewon");
   const [initialDotSize, setInitialDotSize] = useState<number>(0);
-  const [screenSize, setScreenSize] = useState<object>({ width: 0, height: 0 })
+  const [screenSize, setScreenSize] = useState<object>({ width: 0, height: 0 });
   const [ctxState, setCtxState] = useState<CanvasRenderingContext2D>();
   const dotWrapperRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -14,10 +14,10 @@ const App = () => {
   const setDotWrapper = (imgWidth: number, imgHeight: number) => {
     if (dotWrapperRef.current) {
       const dotWrapper = dotWrapperRef.current as HTMLDivElement;
-      dotWrapper.style.width = `${imgWidth}px`
-      dotWrapper.style.height = `${imgHeight}px`
+      dotWrapper.style.width = `${imgWidth}px`;
+      dotWrapper.style.height = `${imgHeight}px`;
     }
-  }
+  };
 
   const imgOnLoad = (e: any) => {
     if (canvasRef.current && e.target) {
@@ -28,29 +28,33 @@ const App = () => {
       const { width, height } = img;
       canvas.width = width;
       canvas.height = height;
-      ctx.drawImage(img, 0, 0, width, height)
+      ctx.drawImage(img, 0, 0, width, height);
       setDotWrapper(width, height);
-      setInitialDotSize(350)
+      setInitialDotSize(350);
     }
-  }
+  };
 
   useEffect(() => {
     setScreenSize({
-      width: window.innerWidth, height: window.innerHeight
-    })
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
     window.addEventListener("resize", () => {
       setScreenSize({
-        width: window.innerWidth, height: window.innerHeight
-      })
-    })
-  }, [])
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    });
+  }, []);
 
   return (
     <div id="main-wrapper">
-      <img onLoad={imgOnLoad} alt="chaewon" src={`/img/${name}.jpg`}></img>
+      <img onLoad={imgOnLoad} alt="chaewon" src={`./img/${name}.jpg`}></img>
       <canvas ref={canvasRef}></canvas>
       <div ref={dotWrapperRef} className="wrapper" id="dot-wrapper">
-        {[1, 2, 3, 4, 5, 6].map(item => <Dot size={initialDotSize} ctx={ctxState} key={item.toString()} />)}
+        {[1, 2, 3, 4, 5, 6].map((item) => (
+          <Dot size={initialDotSize} ctx={ctxState} key={item.toString()} />
+        ))}
       </div>
     </div>
   );
--- src/components/Dot.tsx
+++ src/components/Dot.tsx
@@ -1,8 +1,8 @@
-import React, { useRef, useEffect, useState, memo } from 'react'
+import React, { useRef, useEffect, useState, memo } from "react";
 
 const Dot = memo((props: any) => {
   const [dots, setDots] = useState<Array<any>>([]);
-  const dotRef = useRef<HTMLDivElement>(null)
+  const dotRef = useRef<HTMLDivElement>(null);
 
   const onMouseEnter = (e: any) => {
     if (dotRef.current && e) {
@@ -10,31 +10,37 @@ const Dot = memo((props: any) => {
       dotRef.current.classList.add("wrapper");
       setDots([1, 2, 3, 4]);
     }
-  }
+  };
 
-  useEffect(() => {
-  }, [])
+  useEffect(() => {}, []);
 
   useEffect(() => {
     if (dotRef.current) {
       const dot = dotRef.current;
-      const x = dot.offsetLeft + dot.offsetWidth / 2, y = dot.offsetTop + dot.offsetHeight / 2;
+      const x = dot.offsetLeft + dot.offsetWidth / 2,
+        y = dot.offsetTop + dot.offsetHeight / 2;
       if (props.ctx) {
         const colorData = props.ctx.getImageData(x, y, 1, 1).data;
         dot.style.backgroundColor = `rgb(${colorData[0]},${colorData[1]},${colorData[2]})`;
       }
       if (dot.clientWidth >= 20) {
-        dot.addEventListener("mouseenter", onMouseEnter, { once: true })
+        dot.addEventListener("mouseenter", onMouseEnter, { once: true });
       }
     }
-  }, [props.size, props.ctx])
+  }, [props.size, props.ctx]);
   return (
-    <div ref={dotRef} className="dot" style={{ width: props.size, height: props.size }}>
+    <div
+      ref={dotRef}
+      className="dot"
+      style={{ width: props.size, height: props.size }}
+    >
       {dots.map((item) => {
-        return <Dot size={props.size / 2} key={item.toString()} ctx={props.ctx} />
+        return (
+          <Dot size={props.size / 2} key={item.toString()} ctx={props.ctx} />
+        );
       })}
     </div>
-  )
-})
+  );
+});
 
-export default Dot
+export default Dot;
